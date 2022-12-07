3b4142aa42a651f8a8a636d7697a7f884bc344eb
Chris Grandin <chris.grandin@gmail.com>
2022-12-06 23:49:33 -0800

    Add functions to run queries for spatial catch, add the sql files

---
+++ R/run-spatial-catch-sql.R
@@ -0,0 +1,17 @@
+#' Run SQL queries on the gffos database
+#'
+#' @param fns A vector of filenames of the SQL files continaing
+#' SQL code in plaintext
+#'
+#' @return A list of [tibble::tibble()], one for each query
+#' @export
+run_spatial_catch_sql <- function(fns = here::here("sql",
+                                                   c("catch-locations-ft.sql",
+                                                     "catch-locations-ss.sql",
+                                                     "catch-locations-jv.sql"))){
+
+  purrr::map(fns, ~{
+    gfdata::run_sql("gffos", readr::read_file(.x)) |>
+      tibble::as_tibble()
+  })
+}
