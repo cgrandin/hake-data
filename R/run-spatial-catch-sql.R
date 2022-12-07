16f0b96ca95545e3591c14dc8441587668aff8ff
Chris Grandin <chris.grandin@gmail.com>
2022-12-07 00:14:56 -0800

    Add writing of dat files for spatial catch sql function

--- R/run-spatial-catch-sql.R
+++ R/run-spatial-catch-sql.R
@@ -2,17 +2,35 @@
 #'
 #' @param fns A vector of filenames of the SQL files continaing
 #' SQL code in plaintext
+#' @param write_files If `TRUE`, write the output to dat files
+#' If `FALSE`, return the list of tibbles
 #'
 #' @return A list of [tibble::tibble()], one for each query
 #' @export
 run_spatial_catch_sql <- function(fns = here::here("sql",
                                                    c("catch-locations-ft.sql",
                                                      "catch-locations-ss.sql",
-                                                     "catch-locations-jv.sql"))){
+                                                     "catch-locations-jv.sql")),
+                                  write_files = TRUE){
 
-  purrr::map(fns, ~{
+  lst <- purrr::map(fns, ~{
     gfdata::run_sql("gffos", readr::read_file(.x)) |>
       tibble::as_tibble()
   }) |>
     setNames(c("ft", "ss", "jv"))
+
+
+  if(!write_files){
+    return(lst)
+  }
+
+  out_fns <- gsub("\\.sql", ".dat", basename(fns))
+  walk2(lst, out_fns, ~{
+    # Need as.data.frame() here to avoid quoted headers
+    write_csv(as.data.frame(.x),
+              here::here("data-output",
+                         .y),
+              quote = "none")
+  })
+  invisible()
 }
